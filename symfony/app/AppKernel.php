21b6527bc2050f04e93093089883f66933c80f33
Dan Finnie <dan@danfinnie.com>
2012-11-02 13:38:17 -0400

    Add bundle DanFinnieSecurityBundle

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new DanFinnie\GasBundle\DanFinnieGasBundle(),
+            new DanFinnie\SecurityBundle\DanFinnieSecurityBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ symfony/src/DanFinnie/SecurityBundle/DanFinnieSecurityBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DanFinnie\SecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DanFinnieSecurityBundle extends Bundle
+{
+}
